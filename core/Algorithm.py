a3796017730a47db36db75ea1f50a8fb48428e0f
LouisSarthou <lsarthou@enseirb-matmeca.fr>
2019-12-08 22:05:57 +0100

    Lire et écrire un algorithme dans un fichier

--- core/Algorithm.py
+++ core/Algorithm.py
@@ -6,24 +6,35 @@ move_int_to_str  = {1:"R",2:"R'",3:"R2",4:"U",5:"U'",6:"U2",7:"L",8:"L'",9:"L2",
 class Algorithm:
 
     def __init__(self, str=None):
-        self.moves = []
+        self.moves = np.array([])
         if str is not None:
             move_str_list = str.split(" ")
-            for move_str in move_str_list:
+            self.moves = np.empty(len(move_str_list), dtype=int)
+            for i, move_str in enumerate(move_str_list):
                 n = move_str_to_int[move_str]
-                self.moves.append(n)
+                self.moves[i] = n
 
     def tofile(self, f):
-        np.save(f, self.moves)
+        assert f.mode[0] == "a"
+        assert not f.closed
+        np.array([len(self.moves)]).tofile(f)
+        self.moves.tofile(f)
 
+    def fromfile(self, f):
+        assert f.mode[0] == "r"
+        assert not f.closed
+        n_moves = np.fromfile(f, dtype=int, count=1)[0]
+        self.moves = np.fromfile(f, dtype=int, count=n_moves)
 
-
-    def __str__(self):
+    def __repr__(self):
         str = ""
         for n in self.moves:
             str += move_int_to_str[n] + " "
         str = str[:-1]
         return str
+
+    def __getitem__(self, k):
+        return move_int_to_str[self.moves[k]]
 
     def __iter__(self):
         return iter(self.moves)
--- main.py
+++ main.py
@@ -2,18 +2,32 @@ from core.CubeState import *
 from core.Algorithm import *
 import time
 
-scramble = "D2 F2 U' F2 U R2 D' L2 U F2 D F D2 L B2 U2 R B' D U F2"
-alg = Algorithm(scramble)
-c = CubeState()
-c = c.applyAlg(alg)
+# scramble = "D2 F2 U' F2 U R2 D' L2 U F2 D F D2 L B2 U2 R B' D U F2"
+# alg = Algorithm(str=scramble)
+# c = CubeState()
+# c = c.applyAlg(alg)
+# print(alg)
+# print(c)
 
-print(c)
+# Efface les fichiers
+f=open("maneuvers/0/cubes.dat", "w")
+f.close()
+f=open("maneuvers/0/algorithms.dat", "w")
+f.close()
 
-with open("cube.dat", "a") as f:
-    c.tofile(f)
+with open("maneuvers/0/cubes.dat", "a") as f_cubes:
+    with open("maneuvers/0/algorithms.dat", "a") as f_algs:
+        for move in all_moves:
+            move_alg = Algorithm(move_int_to_str[move])
+            move_alg.tofile(f_algs)
+            all_moves[move].tofile(f_cubes)
 
-a = CubeState()
-with open("cube.dat", "r") as f:
-    a.fromfile(f)
-
-print(a)
+algo = Algorithm()
+cu   = CubeState()
+with open("maneuvers/0/algorithms.dat", "r") as f_algs:
+    with open("maneuvers/0/cubes.dat", "r") as f_cubes:
+        for i in range(18):
+            algo.fromfile(f_algs)
+            cu.fromfile(f_cubes)
+            print(algo)
+            print(cu)
