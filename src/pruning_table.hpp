3b4126be338b4d36a0b9a9fea34df127a9664bab
Louis Sarthou <louissarthou@gmail.com>
2025-07-11 17:02:20 +0200

    Generic PruningTable type

---
+++ src/pruning_table.hpp
@@ -0,0 +1,107 @@
+#pragma once
+#include <algorithm>  // std::fill(begin, end)
+#include <cstdint>    // uint8_t
+#include <deque>      // std::deque
+#include <filesystem> // locate table files
+#include <fstream>    // write tables into files
+#include <limits>     // std::numeric_limits
+#include <memory>     // std::shared_ptr
+
+#include "coordinate.hpp"
+#include "move.hpp"
+#include "utils.hpp"
+
+namespace fs = std::filesystem;
+
+template <std::size_t N> struct PruningTable {
+  using entry_type = uint8_t; // Cannot be printed, max representable
+  // value of uint8_t is 255
+  static constexpr unsigned unassigned = std::numeric_limits<entry_type>::max();
+
+  std::shared_ptr<entry_type[]> table{new entry_type[N]};
+
+  PruningTable() { std::fill(table.get(), table.get() + N, unassigned); }
+
+  unsigned estimate(const unsigned &index) const {
+    assert(index < N);
+    return table[index];
+  }
+
+  void set(const unsigned &index, const unsigned &value) {
+    assert(index < N);
+    assert(value < unassigned); // Ensure value fits in uint8_t
+    table[index] = static_cast<entry_type>(value);
+  }
+
+  void reset() { std::fill(table.get(), table.get() + N, unassigned); }
+
+  auto get_estimator() const {
+    return [this](const unsigned &index) { return this->estimate(index); };
+  }
+
+  unsigned size() const { return N; }
+
+  entry_type &operator[](const unsigned k) const {
+    assert(k < N);
+    return table[k];
+  }
+
+  bool is_assigned(const unsigned &index) const {
+    assert(index < N);
+    return table[index] != unassigned;
+  }
+
+  bool is_filled() const {
+    // Check if all entries are assigned
+    return std::all_of(table.get(), table.get() + N,
+                       [](const entry_type &e) { return e != unassigned; });
+  }
+
+  template <typename Cube, typename Mover, typename Indexer,
+            std::size_t NM = 18>
+  auto generate(const Mover &apply, const Indexer &index,
+                const std::array<Move, NM> &moves = HTM_Moves) {
+    std::deque<Cube> queue{Cube()};
+
+    table[index(Cube())] = 0;
+
+    while (!queue.empty()) {
+      Cube cc = queue.back();
+
+      unsigned i = index(cc);
+      unsigned depth = table[i];
+      assert(i < N);
+
+      for (auto move : moves) {
+        Cube cc2 = cc;
+        apply(move, cc2);
+
+        unsigned ii = index(cc2);
+        assert(ii < N);
+        if (!is_assigned(ii)) {
+          table[ii] = depth + 1;
+          queue.push_front(cc2);
+        }
+      }
+      queue.pop_back();
+    }
+  }
+
+  void load(fs::path table_path) {
+    if (fs::exists(table_path)) {
+      std::ifstream istrm(table_path, std::ios::binary);
+      istrm.read(reinterpret_cast<char *>(table.get()), sizeof(entry_type) * N);
+      istrm.close();
+    } else {
+      print("Pruning table not found at: ", table_path);
+    }
+  }
+
+  void write(fs::path table_path) const {
+    fs::create_directories(table_path.parent_path());
+    std::ofstream file(table_path, std::ios::binary);
+    file.write(reinterpret_cast<const char *>(table.get()),
+               sizeof(entry_type) * N);
+    file.close();
+  }
+};
---
+++ test/pruning_table_test.cpp
@@ -0,0 +1,62 @@
+#include "coordinate.hpp"
+#include "cubie_cube.hpp"
+#include "pruning_table.hpp"
+#include <cassert>
+#include <iostream>
+
+void test_api() {
+  PruningTable<10> pt;
+
+  assert(pt.size() == 10);
+
+  // Test initial state
+  for (unsigned i = 0; i < pt.size(); ++i) {
+    assert(pt.estimate(i) == PruningTable<10>::unassigned);
+  }
+
+  // Set some values
+  pt.set(0, 5);
+  pt.set(1, 3);
+  assert(pt.estimate(0) == 5);
+  assert(pt.estimate(1) == 3);
+
+  // Check is_assigned
+  assert(pt.is_assigned(0));
+
+  pt.reset();
+  assert(pt.estimate(0) == PruningTable<10>::unassigned);
+  assert(pt.estimate(1) == PruningTable<10>::unassigned);
+
+  auto estimator = pt.get_estimator();
+  pt.set(1, 3);
+  assert(estimator(0) == PruningTable<10>::unassigned);
+  assert(estimator(1) == 3);
+}
+
+void test_generate() {
+  // Pruning table to solve cp
+  constexpr auto N = factorial(8);
+  PruningTable<N> pt;
+
+  auto apply = [](const Move &move, CubieCube &cc) { cc.apply(move); };
+  auto index = [](const CubieCube &cc) { return permutation_index(cc.cp); };
+
+  pt.generate<CubieCube>(apply, index);
+
+  assert(pt.is_filled());
+
+  pt.write("pruning_tables/all_corners.dat");
+  PruningTable<N> reload;
+  reload.load("pruning_tables/all_corners.dat");
+
+  assert(reload.is_filled());
+  for (unsigned i = 0; i < N; ++i) {
+    assert(reload.estimate(i) == pt.estimate(i));
+  }
+}
+
+int main() {
+  test_api();
+  test_generate();
+  return 0;
+}
