36b4fc96e96e4c3e4268ea107dc360723ec7264f
Louis Sarthou <louissarthou@gmail.com>
2025-07-29 16:43:13 +0200

    MoveTable implementation

---
+++ src/move_table.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <algorithm>   // std::fill, std::all_of
+#include <filesystem>  // locate table files
+#include <fstream>     // write tables into files
+#include <memory>      // std::shared_ptr
+
+#include "cubie_cube.hpp"
+#include "move.hpp"
+
+namespace fs = std::filesystem;
+
+template <unsigned N>
+struct MoveTable {
+    std::shared_ptr<unsigned[]> table{new unsigned[N * N_HTM_MOVES]};
+
+    fs::path move_table_dir() const { return "move_tables/"; }
+
+    MoveTable() { std::fill(table.get(), table.get() + N, N); }
+
+    template <typename Indexer, typename Unindexer>
+    void compute(const Indexer &index, const Unindexer &from_index) {
+        // index : a function that takes in a CubieCube instance and returns
+        // some coordinate for it from_index : a function that takes in a
+        // coordinate value and returns a CubieCube instance for it
+
+        for (unsigned c = 0; c < N; ++c) {
+            CubieCube cc = from_index(c);
+            for (Move m : HTM_Moves) {
+                auto buffer_cc = cc;
+                buffer_cc.apply(m);
+                table[c * N_HTM_MOVES + m] = index(buffer_cc);
+            }
+        }
+    }
+
+    bool is_filled() const {
+        // Check if all entries are assigned
+        return std::all_of(table.get(), table.get() + N * N_HTM_MOVES,
+                           [](const unsigned &e) { return e < N; });
+    }
+
+    void apply(unsigned &c, const Move &m) { c = table[c * N_HTM_MOVES + m]; }
+
+    bool load(fs::path filename) {
+        auto table_path = move_table_dir() / filename;
+        if (fs::exists(table_path)) {
+            std::ifstream istrm(table_path, std::ios::binary);
+            istrm.read(reinterpret_cast<char *>(table.get()),
+                       sizeof(unsigned) * N * N_HTM_MOVES);
+            istrm.close();
+            assert(is_filled());
+            return true;
+        } else {
+            print("Move table not found at: ", table_path);
+        }
+        return false;
+    }
+
+    void write(fs::path filename) const {
+        auto table_path = move_table_dir() / filename;
+        fs::create_directories(table_path.parent_path());
+        std::ofstream file(table_path, std::ios::binary);
+        file.write(reinterpret_cast<const char *>(table.get()),
+                   sizeof(unsigned) * N * N_HTM_MOVES);
+        file.close();
+    }
+};
--- test/coordinate_test.cpp
+++ test/coordinate_test.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include "cubie.hpp"
+#include "move_table.hpp"
 #include "utils.hpp"
 
 void test_layout_index() {
@@ -51,9 +52,45 @@ void test_co_index() {
     }
 }
 
+void test_move_table() {
+    constexpr unsigned N = factorial(NC);
+    MoveTable<N> move_table;
+
+    auto index = [](const CubieCube &cc) { return permutation_index(cc.cp); };
+    auto from_index = [](const unsigned &c) {
+        CubieCube ret;
+        permutation_from_index(c, ret.cp);
+        return ret;
+    };
+
+    move_table.compute(index, from_index);
+    move_table.write("corners");
+    assert(move_table.is_filled());
+
+    CubieCube cc;
+    unsigned c;
+    for (Move m : {R, U3, F2, B, D3, L2}) {
+        cc.apply(m);
+        move_table.apply(c, m);
+        assert(c == index(cc));
+    }
+
+    MoveTable<N> loaded_move_table;
+    assert(loaded_move_table.load("corners"));
+
+    // Check that the loaded table is the same as the original
+    for (unsigned c = 0; c < N; ++c) {
+        for (Move m : HTM_Moves) {
+            assert(move_table.table[c * N_HTM_MOVES + m] ==
+                   loaded_move_table.table[c * N_HTM_MOVES + m]);
+        }
+    }
+}
+
 int main() {
     test_layout_index();
     test_permutation_index();
     test_eo_index();
     test_co_index();
+    test_move_table();
 }
